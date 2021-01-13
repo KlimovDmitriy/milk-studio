890abf8e13beca9d80bb29a3689765fb98a03a61
Dmitriy Klimov <d.klimov@web-nanny.ru>
2021-01-14 00:48:13 +0300

    add composer and migrator

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,3 +1,8 @@
 <?php
+require(__DIR__.'/../vendor/autoload.php');
 
-?>
+use Bitrix\Main\EventManager;
+
+if(defined('ADMIN_SECTION')){
+    //Arrilot\BitrixMigrations\Autocreate\Manager::init($_SERVER["DOCUMENT_ROOT"].'/local/migrations');
+}
