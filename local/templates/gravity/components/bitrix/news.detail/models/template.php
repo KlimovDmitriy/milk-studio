cbe8039ecdc7479fe56b26f295f1d720a1f7c381
Dmitriy Klimov <d.klimov@web-nanny.ru>
2021-01-15 00:34:40 +0300

    Обновлены стили

--- local/templates/gravity/components/bitrix/news.detail/models/template.php
+++ local/templates/gravity/components/bitrix/news.detail/models/template.php
@@ -1,8 +1,9 @@
 <?php
 ?>
 <!--<pre>-->
-<!--    --><?//print_r($arResult['PROPERTIES'])?>
+<!--    --><?// print_r($arResult['PROPERTIES'])?>
 <!--</pre>-->
+<div class="row">
     <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
             <a class="nav-link active" id="home-tab" data-bs-toggle="tab" href="#logins" role="tab" aria-controls="home"
@@ -13,7 +14,17 @@
                aria-selected="false">Заметки</a>
         </li>
     </ul>
-    <div class="tab-content" id="myTabContent">
-        <div class="tab-pane fade show active" id="logins" role="tabpanel" aria-labelledby="home-tab"><?= $arResult['PROPERTIES']['LOGINS']['~VALUE']['TEXT'];?></div>
-        <div class="tab-pane fade" id="notes" role="tabpanel" aria-labelledby="profile-tab">...</div>
+</div>
+<div class="tab-content" id="myTabContent">
+    <div class="tab-pane fade show active" id="logins" role="tabpanel"
+         aria-labelledby="home-tab">
+        <div class="row"><?= $arResult['PROPERTIES']['LOGINS']['~VALUE']['TEXT']; ?></div>
     </div>
+    <div class="tab-pane fade" id="notes" role="tabpanel"
+         aria-labelledby="profile-tab">
+        <div class="row mt-3"><?= $arResult['PROPERTIES']['NOTES']['~VALUE']['TEXT']; ?></div>
+        <div class="row">
+            <button type="button" class="btn btn-warning">Обновить заметки</button>
+        </div>
+    </div>
+</div>
