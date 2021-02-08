ba0aab16b336863f0271dd99d2ef7e199a7aac02
Dmitriy Klimov <klimov@racurs.agency>
2021-02-08 12:53:29 +0300

    Непомню уже что сделал

--- local/templates/gravity/components/bitrix/news.list/models/template.php
+++ local/templates/gravity/components/bitrix/news.list/models/template.php
@@ -7,7 +7,16 @@ $userId = $USER->GetID();
 <?php foreach ($arResult['ITEMS'] as $arItem): ?>
     <?php
     if ($userId == '1' || $arItem['PROPERTIES']['MODEL_ID']['VALUE'] == $userId):?>
-        <a href="<?=$arItem['DETAIL_PAGE_URL']?>" class="list-group-item list-group-item-action"><?=$arItem['NAME']?></a>
+        <a href="<?=$arItem['DETAIL_PAGE_URL']?>" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"><?=$arItem['NAME']?><span class="btn btn-danger float-right d">Деактивировать</span></a>
     <?php endif;?>
 <?php endforeach; ?>
 </div>
+
+<script>
+    $(document).ready(function (){
+        $(".d").click(function(e){
+            e.preventDefault();
+            console.log('123')
+        })
+    })
+</script>
